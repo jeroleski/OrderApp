9f4fb3ae059e01458b401cab3bf212c91edf279b
JackITU <jkrs@itu.dk>
2024-03-26 19:53:00 +0100

    Start page layout

---
+++ app/src/main/java/com/example/orderapp/MenuDb.kt
@@ -0,0 +1,35 @@
+package com.example.orderapp
+
+class Product(val name: String, val prize: Int)
+class Category(val type: String, val products: List<Product>)
+class Menu(val categories: List<Category>)
+
+object MenuDb {
+    var menu: Menu = Menu(listOf())
+}
+
+fun mockData(db: MenuDb) {
+    val p1 = Product("Pizza Margarita", 51)
+    val p2 = Product("Pizza Pepperoni ", 52)
+    val p3 = Product("Pizza Hawaii", 53)
+    val p4 = Product("Pizza Parma", 54)
+    val p5 = Product("Pizza Potato", 55)
+    val pizza = Category("Pizza", listOf(p1, p2, p3, p4, p5))
+
+    val d1 = Product("Tiramisu", 40)
+    val d2 = Product("Apple Pie", 50)
+    val d3 = Product("Banana Split", 60)
+    val d4 = Product("Neapolitan Ice Cream", 70)
+    val d5 = Product("Cheese Slate", 80)
+    val dessert = Category("Dessert", listOf(d1, d2, d3, d4, d5))
+
+    val s1 = Product("Coca Cola", 20)
+    val s2 = Product("Sprite", 22)
+    val s3 = Product("Orange Juice", 20)
+    val s4 = Product("Iced Tea", 20)
+    val s5 = Product("Pina Colada", 20)
+    val drink = Category("Drinks", listOf(s1, s2, s3, s4, s5))
+
+    val menu = Menu(listOf(pizza, dessert, drink))
+    db.menu = menu
+}
